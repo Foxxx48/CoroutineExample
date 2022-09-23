d03580d1e1f6b082d3ad2fb1ee3c3d4802300158
Foxxx48 <Foxxdim@mail.ru>
2022-09-23 14:08:53 +0300

    Use Deprecated Handler()

--- app/src/main/java/com/fox/coroutineexample/MainActivity.kt
+++ app/src/main/java/com/fox/coroutineexample/MainActivity.kt
@@ -2,6 +2,7 @@ package com.fox.coroutineexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Toast
 import androidx.core.view.isVisible
 import com.fox.coroutineexample.databinding.ActivityMainBinding
@@ -12,12 +13,15 @@ class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
 
+    val handler = Handler()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.btnDownload.setOnClickListener {
+            hideParams()
             loadData()
         }
     }
@@ -39,25 +43,40 @@ class MainActivity : AppCompatActivity() {
     private fun loadCity(callback: (String) -> Unit) {
         thread {
             Thread.sleep(5000)
+            handler.post {
+                callback.invoke("Moscow")
+               binding.progressBar.isVisible = false
+            }
 
-            callback.invoke("Moscow")
         }
 
     }
 
     private fun loadTemperature(city: String, callback: (Int) -> Unit) {
         thread {
-            Toast.makeText(
-                this,
-                getString(R.string.loading_temperature_toast, city),
-                Toast.LENGTH_SHORT
-            ).show()
-
+            Thread.sleep(1500)
+            handler.post {
+                binding.progressBar.isVisible = true
+                Toast.makeText(
+                    this,
+                    getString(R.string.loading_temperature_toast, city),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
             Thread.sleep(5000)
-
-            callback.invoke(17)
+            handler.post {
+                callback.invoke(17)
+            }
         }
+    }
 
+    private fun hideParams() {
+        if (binding.tvCityName != null) {
+            binding.tvCityName.text = ""
+        }
+        if (binding.tvTempValue != null) {
+           binding.tvTempValue.text = ""
+        }
     }
 
     override fun onDestroy() {
